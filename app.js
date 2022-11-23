8b84a018cf0e86a223062e5664864ff1f8a1c75c
russianhorror <60624368+russianhorror@users.noreply.github.com>
2022-11-23 15:53:31 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+	
+function bglowdiv(subjects) {
+  for (var testimonialText of subjects) {
+    testimonialText.insertAdjacentHTML('afterend', '<div class="bottom_glow"> </div>');
+  }
+}
+
+var subjects = document.querySelectorAll('.catalog__product-content');
+bglowdiv(subjects);
+	
+	
+
 particlesJS.load('particles-js', 'particlesjs-config.json');
 
 // Cursor
